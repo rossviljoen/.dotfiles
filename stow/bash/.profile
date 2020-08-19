03f69bc3b9929205ee31c1facb6f062b614f3f06
Ross Viljoen <ross@viljoen.co.uk>
2020-08-19 20:58:35 +0100

    Updates

--- stow/bash/.profile
+++ stow/bash/.profile
@@ -1,5 +1,6 @@
-export EDITOR=emacsclient -c -a emacs
-export VISUAL=emacsclient -c -a emacs
+export EDITOR="emacsclient -c -a emacs"
+export VISUAL="emacsclient -c -a emacs"
 
 export PATH=$PATH:$HOME/programming/go/bin
 export GOPATH=$HOME/programming/go:$HOME/programming/titan/gocode
+export GOOGLE_APPLICATION_CREDENTIALS="/home/ross/google/service-account.json"
--- stow/emacs/.emacs.d/init.el
+++ stow/emacs/.emacs.d/init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("99c86852decaeb0c6f51ce8bd46e4906a4f28ab4c5b201bdc3fdf85b24f88518" "527df6ab42b54d2e5f4eec8b091bd79b2fa9a1da38f5addd297d1c91aa19b616" default)))
+    ("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "99c86852decaeb0c6f51ce8bd46e4906a4f28ab4c5b201bdc3fdf85b24f88518" "527df6ab42b54d2e5f4eec8b091bd79b2fa9a1da38f5addd297d1c91aa19b616" default)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -16,8 +16,8 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; Straight package manager
 (setq straight-use-package-by-default t)
-
 ;; Bootstrap straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -31,48 +31,17 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
 (straight-use-package 'use-package)
 
-;; (use-package better-defaults
-;;   :config
-;;   (ido-mode nil))
-(use-package pdf-tools
-  :config
-  (pdf-tools-install))
-(use-package auctex
-  :defer t
-  :config
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  (setq TeX-view-program-selection
-   (quote
-    (((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "PDF Tools")
-     (output-html "xdg-open"))))
-  (setq-default TeX-master nil)
-   ;; revert pdf-view after compilation TODO: I don't think this works
-  (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer))
-(use-package transpose-frame)
-(use-package smooth-scrolling
-	     :config
-	     (smooth-scrolling-mode t))
-
-;; Nix/NixOS development
+;; Nix/NixOS
 (use-package nix-mode)
-;; (use-package direnv
-;;   :config
-;;   (direnv-mode))
 
+;; Haskell
 (use-package haskell-mode
   :config
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
 
-;; OCaml development
+;; OCaml
 (use-package tuareg
   :config
   (setq tuareg-prettify-symbols-full t)
@@ -89,20 +58,91 @@
   (defun utop--supports-company ()      ; Fixes dune utop not using company
     (featurep 'company)))
 
-;; Go development
+;; Golang
 (use-package go-mode
   :config
-  (setenv "GOPATH" (concat (getenv "HOME") "/programming/go"))
-  (add-to-list 'exec-path (concat (file-name-as-directory (getenv "GOPATH")) "bin") t)
-  (add-hook 'go-mode-hook (lambda ()
-                            (which-function-mode)
-                            (add-hook 'before-save-hook 'gofmt-before-save)))
-  )
+  (setenv "GOPATH" (concat (getenv "HOME") "/programming/go" ":" (getenv "HOME") "/programming/titan/gocode"))
+  (add-to-list 'exec-path (concat (getenv "HOME") "/programming/go/bin") t)
+  (defun lsp-go-install-save-hooks ()
+    (add-hook 'before-save-hook #'lsp-format-buffer t t)
+    (add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
 
+;; Julia
+(use-package julia-mode)
+
+;; Latex
+(use-package auctex
+  :defer t
+  :ensure t
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq TeX-view-program-selection
+   (quote
+    (((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "PDF Tools")
+     (output-html "xdg-open"))))
+  (setq-default TeX-master nil)
+   ;; revert pdf-view after compilation TODO: I don't think this works
+  (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer))
+
+;; LSP
+(use-package lsp-mode
+  :init (setq lsp-keymap-prefix "C-c")
+  :commands (lsp lsp-deferred)
+  :hook
+  (go-mode . lsp-deferred)
+  (lsp-mode . lsp-enable-which-key-integration)
+  :config
+  (setq lsp-gopls-env
+        (list (cons "GOPATH" (getenv "GOPATH")) (cons "GO111MODULE" "on")))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-tramp-connection
+                                     (lambda() (cons "/home/ross/go/bin/gopls"
+                                                     lsp-gopls-server-args)))
+                    :major-modes '(go-mode go-dot-mod-mode)
+                    :priority 10
+                    :remote? t
+                    :server-id 'gopls-remote)))
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-enable nil))
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+
+(use-package direnv
+  :config
+  (direnv-mode))
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-copy-env "GOOGLE_APPLICATION_CREDENTIALS")
+  (exec-path-from-shell-copy-env "GOPATH"))
+
+(use-package vterm)
+(use-package vterm-toggle
+  :bind
+  ("M-<return>" . vterm-toggle)
+  ("S-M-<return>" . vterm-toggle-cd))
+
+(use-package which-key :config (which-key-mode))
+
+(use-package pdf-tools
+  :config
+  (pdf-tools-install))
+(use-package transpose-frame)
+(use-package smooth-scrolling
+	     :config
+	     (smooth-scrolling-mode t))
 (use-package sudo-edit)
 (use-package base16-theme
   :config
-  (load-theme (intern (or (getenv "THEME") "base16-solarized-light")) t)) ;; Try to load a theme from env variable
+  ;; Try to load a theme from env variable
+  (load-theme 'base16-solarized-light t))
 (use-package visual-fill-column)
 (use-package speed-type)
 (use-package counsel
@@ -131,40 +171,37 @@
    ("C-c L" . counsel-git-log)
    ("C-x l" . counsel-locate)
    ("C-S-o" . counsel-rhythmbox)
-   ("<C-tab>" . counsel-company)
+   ;; ("<C-tab>" . counsel-company)
    ;; ("C-c k" . counsel-ag)
    ))
 (use-package magit
   :bind
   (("C-x g" . magit-status)))
-;; (use-package frames-only-mode
-;;   :config
-;;   (frames-only-mode t))
 (use-package company ;; Autocomplete
   :config
   (global-company-mode t)
   (setq company-show-numbers 't)
-  ;; :hook
-  ;; (nix-mode . (lambda() (setq-local company-backends (company-lsp :with company-dabbrev :with company-yasnippet :with company-files))))
-  )
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  :bind
+  ("<C-tab>" . company-complete))
 (use-package ivy-prescient
   :config
   (ivy-prescient-mode t)
   (prescient-persist-mode t))
-;; (use-package company-nixos-options ;; Seems pretty broken
-;;   :config
-;;   (add-to-list 'company-backends 'company-nixos-options))
-;; (use-package flycheck)
+(use-package flycheck)
 ;; (use-package elpy
 ;;   :init
 ;;   (elpy-enable))
-(use-package julia-mode)
+
 (use-package jupyter
   :bind
   (("C-c C-e" . jupyter-eval-line-or-region)
    ("C-c C-r" . jupyter-eval-region)
    ("C-c C-b" . jupyter-eval-buffer)
-   ("C-c C-f" . jupyter-eval-defun)))
+   ("C-c C-f" . jupyter-eval-defun))
+  :config
+  (setq jupyter-repl-echo-eval-p t))
 (use-package rainbow-mode)
 (use-package native-complete
   :init
@@ -192,6 +229,7 @@
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq dired-dwim-target t)
+(setq dired-listing-switches "-alh")
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode t)
 (global-set-key (kbd "M-SPC") 'cycle-spacing)
@@ -202,21 +240,26 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
+;; Tramp config
+(setq tramp-auto-save-directory "~/.emacs.d/var/tramp/")
+(setq tramp-chunksize 2000)
+(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
 (connection-local-set-profile-variables
  'remote-bash
  '((shell-file-name . "/bin/bash")
    (shell-command-switch . "-c")
    (shell-interactive-switch . "-i")
    (shell-login-switch . "-l")))
-
 (connection-local-set-profiles
  '(:application tramp :protocol "ssh" :machine "dfly")
  'remote-bash)
 
 ;; Ibuffer configuration
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 (setq ibuffer-show-empty-filter-groups nil)
 (setq ibuffer-saved-filter-groups
       '(("Home"
+         ("Programming" (mode . go-mode))
          ("Dired" (mode . dired-mode))
          ("Org" (or (name . "^.*org$")
                     (mode . org-mode)))
@@ -227,8 +270,7 @@
          ("Magit" (or (name . "^magit*") (mode . magit-mode)))
          ("Help" (or (name . "\*Help\*")
                      (name . "\*Apropos\*")
-                     (name . "\*info\*")))
-         )))
+                     (name . "\*info\*"))))))
 (add-hook 'ibuffer-mode-hook
           '(lambda ()
              (ibuffer-auto-mode 1)
